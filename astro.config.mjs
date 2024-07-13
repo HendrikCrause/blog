07b5eb1c345f2c9eae67f9b77b07f3f3a1a76fe0
HendrikCrause <wimpie.crause@gmail.com>
2024-07-13 17:26:36 +0200

    New base take 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hendrikcrause.github.io/blog',
+	base: 'blog',
 	integrations: [mdx(), sitemap()],
 });
