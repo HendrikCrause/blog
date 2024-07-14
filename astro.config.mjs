c952508d52a334bcb5bc6f5889bcb0103521b79d
HendrikCrause <wimpie.crause@gmail.com>
2024-07-14 06:07:03 +0200

    Get links to work with base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hendrikcrause.github.io/blog',
-	base: 'blog',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://hendrikcrause.github.io',
+  base: '/blog',
+  integrations: [mdx(), sitemap()],
+})
